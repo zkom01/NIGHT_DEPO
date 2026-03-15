1169a3d1c932fe4ab13de03073906ad9c91b94c5
zkom01 <komarek.zdenek@seznam.cz>
2026-03-15 01:37:43 +0100

    odstranění java scriptu pro mizení hlášek a nahrazení kódem v css

--- assets/header.php
+++ assets/header.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- přidáme styl pro hlavičku s verzováním pro zajištění načítání aktualizovaného souboru -->
+
+    <!-- styl pro hlavičku s verzováním pro zajištění načítání aktualizovaného souboru -->
     <link rel="stylesheet" href="./css/header.css?v=<?php echo filemtime('./css/header.css'); ?>"> 
-    <link rel="stylesheet" href="css/warning.css"> <!-- přidáme styl pro varování -->
-    <link rel="stylesheet" href="css/message.css"> <!-- přidáme styl pro hlášky -->
+
+    <link rel="stylesheet" href="css/warning.css?v=<?php echo filemtime('./css/warning.css'); ?>"> <!-- styl pro varování -->
+    <link rel="stylesheet" href="css/message.css?v=<?php echo filemtime('./css/message.css'); ?>"> <!-- styl pro hlášky -->
     <title>
         <?php 
-            // Pokud existuje proměnná $pageTitle, vypiš ji, jinak vypiš "Můj Web"
+            // Pokud existuje proměnná $pageTitle, vypiš ji, jinak vypiš "!!!!!!! DOPLNIT_NÁZEV_STRÁNKY !!!!!!!"
             echo isset($pageTitle) ? $pageTitle : "!!!!!!! DOPLNIT_NÁZEV_STRÁNKY !!!!!!!"; 
         ?>
     </title>
